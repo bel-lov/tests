7982b4174f7517d6598c09ceb6f04e8a7a5fd284
bel-lov <belou.lyuba@yandex.ru>
2022-08-09 16:18:54 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
+export function calculateLife(lifes) {
+    if (lifes.health > 50) {
+        return 'green';
+    }
+    else if (lifes.health < 50 && lifes.health > 15) {
+        return 'yellow';
+    }
+    else if (lifes.health < 15) {
+        return 'red';
+    }
+}
+
 // TODO: write your code here
-import sum from './basic';
+// import sum from './basic';
 
-console.log('worked');
+// console.log('worked');
 
-console.log(sum([1, 2]));
+// console.log(sum([1, 2]));
