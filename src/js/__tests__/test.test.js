1585192eb491c171d8ecc1cf48eb2a4c3cd48ac1
bel-lov <belou.lyuba@yandex.ru>
2022-08-20 22:23:49 +0300

    second commit

--- src/js/__tests__/test.test.js
+++ src/js/__tests__/test.test.js
@@ -1,31 +1,26 @@
-import {calculateLife} from '../app.js'
-
-// test ('basik test', ()=>{
-//     let result = 4;
-//     expect(result).toBe(4);
-// });
+import { calculateLife } from '../app.js';
 
 test('calculateLifeGreen', () => {
-    const person = {
-        name: 'Маг',
-        health: 90
-    };
-    const result = calculateLife(person);
-    expect(result).toBe('green');
-})
+  const person = {
+    name: 'Маг',
+    health: 90,
+  };
+  const result = calculateLife(person);
+  expect(result).toBe('green');
+});
 test('calculateLifeYellow', () => {
-    const person = {
-        name: 'Маг',
-        health: 40
-    };
-    const result = calculateLife(person);
-    expect(result).toBe('yellow');
-})
+  const person = {
+    name: 'Маг',
+    health: 40,
+  };
+  const result = calculateLife(person);
+  expect(result).toBe('yellow');
+});
 test('calculateLifeRed', () => {
-    const person = {
-        name: 'Маг',
-        health: 10
-    };
-    const result = calculateLife(person);
-    expect(result).toBe('red');
-})
+  const person = {
+    name: 'Маг',
+    health: 10,
+  };
+  const result = calculateLife(person);
+  expect(result).toBe('red');
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,11 @@
 export function calculateLife(lifes) {
-    if (lifes.health > 50) {
-        return 'green';
-    }
-    else if (lifes.health < 50 && lifes.health > 15) {
-        return 'yellow';
-    }
-    else if (lifes.health < 15) {
-        return 'red';
-    }
+  if (lifes.health > 50) {
+    return 'green';
+  }
+  if (lifes.health < 50 && lifes.health > 15) {
+    return 'yellow';
+  }
+  if (lifes.health < 15) {
+    return 'red';
+  }
 }
-
-// TODO: write your code here
-// import sum from './basic';
-
-// console.log('worked');
-
-// console.log(sum([1, 2]));
